db425c92956dbb1406b4725af130d91521f65b76
Codegem <117828916+azeezabass2005@users.noreply.github.com>
2023-12-25 21:57:16 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -52,22 +52,22 @@ const Navbar = () => {
                 <div className={`absolute left-0 ${showNav ? '' : '-translate-x-full'}  flex justify-start vs:pl-10 pl-2 xs:p-5 p-2 xs:top-[81px] vs:top-[77px] top-[68px] bg-[#ffe] w-[100%] min-h-svh transform transition-all duration-500 ease-in-out`}>
                     <ul className='flex flex-col vs:gap-8 gap-2 w-full'>
                         {navLinks.map((navlink, index) => (
-                            <li key={index} className='font-semibold hover:text-[goldenrod] hover:font-bold border-b-[goldenrod] border-b-2 py-2 px-1'>
+                            <li onClick={handleNavToggle} key={index} className='font-semibold hover:text-[goldenrod] hover:font-bold border-b-[goldenrod] border-b-2 py-2 px-1'>
                                 {index === navLinks.length - 1 ?  (<NavLink to={navlink.link}>
                                     {navlink.name}
                                 </NavLink>) : (<a href={navlink.link}>{navlink.name}</a>)}
                             </li>
                         ))}
-                        <li className='font-semibold hover:text-[goldenrod] hover:font-bold border-b-[goldenrod] border-b-2 py-2 px-1'>
+                        <li onClick={handleNavToggle} className='font-semibold hover:text-[goldenrod] hover:font-bold border-b-[goldenrod] border-b-2 py-2 px-1'>
                             <a href="#contact">
                                 Contact
                             </a>
                         </li>
                         <div className='flex gap-4  border-b-[goldenrod] border-b-2 pb-3 px-1'>
-                            <a href='#contactus' className='border-[goldenrod] border-2 py-2 px-4 rounded-lg text-[#000] font-semibold'>
+                            <a onClick={handleNavToggle} href='#contactus' className='border-[goldenrod] border-2 py-2 px-4 rounded-lg text-[#000] font-semibold'>
                                 Contact Us
                             </a>
-                            <Link to={'/donation'} className='py-3 px-4 rounded-lg text-[#fff] make-donation duration-[1s] font-semibold'>
+                            <Link onClick={handleNavToggle} to={'/donation'} className='py-3 px-4 rounded-lg text-[#fff] make-donation duration-[1s] font-semibold'>
                                 Make a Donation
                             </Link>
                         </div>
